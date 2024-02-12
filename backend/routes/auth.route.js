289fb25673f9616aeec02d4194e789fe59791a9d
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-02-12 17:39:53 +0100

    save all this changes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signin } from "../controller/controller.auth.js";
+import { login, signin } from "../controller/controller.auth.js";
 const router = express.Router();
 
 router.post("/signin", signin);
+router.post("/login", login);
 
 export default router;
